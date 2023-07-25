12435cbbec02cfac2f112b9279ed2bbfb0090872
hadron-liu <59247756+hadron-liu@users.noreply.github.com>
2023-07-25 22:04:21 +0800

    sync

--- docs/src/App.js
+++ docs/src/App.js
@@ -23,21 +23,65 @@ function Box (props) {
   )
 }
 
+function Sphere (props) {
+  const mesh = useRef()
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += 0.01
+    mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh {...props} ref={mesh}>
+      <sphereGeometry args={[1, 16, 16]} />
+      <meshStandardMaterial color="lightblue" />
+    </mesh>
+  )
+}
+
+function Cone (props) {
+
+  const mesh = useRef()
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += 0.01
+  })
+
+  return (
+    <mesh {...props} ref={mesh}>
+      <coneGeometry args={[1, 2, 32]} />
+      <meshStandardMaterial color="pink" />
+    </mesh>
+  )
+}
+
+function Cylinder (props) {
+  const mesh = useRef()
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.z += 0.01
+  })
+
+  return (
+    <mesh {...props} ref={mesh}>
+      <cylinderGeometry args={[1, 1, 2, 32]} />
+      <meshStandardMaterial color="teal" />
+    </mesh>
+  )
+}
+
 export default function App () {
   return (
     <Canvas>
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[-1.2, 1, 0]} />
-      <Box position={[-1.2, 2, 0]} />
-      <Box position={[-2.2, 0, 1]} />
-      <Box position={[-3.2, 0, 1]} />
-      <Box position={[1.2, 0, 0]} />
-      <Box position={[1.2, 1, 0]} />
-      <Box position={[1.2, 2, 0]} />
-      <Box position={[2.2, 0, 1]} />
-      <Box position={[3.2, 0, 2]} />
+      <pointLight position={[20, 20, 20]} />
+      {/* <Box position={[0, 0, 0]} /> */}
+      {/* <Box position={[0, 1, 0]} /> */}
+      <Box position={[1, 0, 1]} />
+      {/* <ambientLight intensity={0.5} /> */}
+      <Sphere position={[2, 2, 0]} />
+      <Cone position={[0, 0, 0]} />
+      <Cylinder position={[0, -2, 0]} />
     </Canvas>
   )
 }

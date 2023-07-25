40e44d43b62f32b6a6e66696268a12dbbce8e049
hadron-liu <59247756+hadron-liu@users.noreply.github.com>
2023-07-25 19:15:29 +0800

    sync

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,8 +1,29 @@
+import React from 'react'
+
+function Book (props) {
+  return (
+    <div className="book">
+      <div>
+        <h2>{props.title}</h2>
+        <p>{props.author}</p>
+        <p>{props.description}</p>
+        <p>出版时间:{props.pubdate}</p>
+        <p>评分:{props.rating}</p>
+      </div>
+    </div>
+  )
+}
 
 function App () {
   return (
-    <div className="App">
-      app
+    <div className="app">
+      <Book
+        title="三体"
+        author="刘慈欣"
+        description="漫长的黑暗静待黎明到来......"
+        pubdate="2008-1"
+        rating={4.5}
+      />
     </div>
   )
 }
